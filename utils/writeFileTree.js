fa8e099112df994a3541e2b5e0f0598ae3f3dbae
daguanren21 <1363917353@qq.com>
2023-03-10 18:16:16 +0800

    "feat：提供npm install ，writefile 等功能"

---
+++ utils/writeFileTree.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra')
+
+const path = require('node:path');
+
+function writeFileTree(dir, files) {
+    Object.keys(files).forEach(name => {
+        const filePath = path.join(dir, name)
+        fs.ensureDirSync(path.dirname(filePath))
+        fs.writeFileSync(filePath, files[name])
+    });
+}
+
+module.exports = {
+    writeFileTree
+}
